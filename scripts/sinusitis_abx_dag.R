9ea03824d79aee7b1f1b4be200c72aa59e386c8c
Brendan J. Kelly <brendank@upenn.edu>
2024-03-02 14:14:53 -0500

    Update dag and add figures.

--- scripts/sinusitis_abx_dag.R
+++ scripts/sinusitis_abx_dag.R
@@ -12,8 +12,9 @@ set.seed(16)
 #' draw DAG
 ##########################################
 
-dagify(Rx ~ pt_sx,
+dagify(#Rx ~ pt_sx,
        Rx ~ md_trigger,
+       md_trigger ~ pt_sx,
        appropriate ~ Rx,
        appropriate ~ pt_sx,
        md_trigger ~ U,
@@ -32,8 +33,21 @@ sinusitis_abx_dag |>
   ggdag::geom_dag_node() +
   ggdag::geom_dag_text(nudge_y = -0.2, color = "black") +
   theme_dag() +
-  theme(text = element_text(size = 1))
+  theme(text = element_text(size = 1)) |> 
+  identity() -> p_sinusitis_abx_dag
+p_sinusitis_abx_dag
 
+p_sinusitis_abx_dag |> 
+  ggsave(plot = _, filename = "figs/p_sinusitis_abx_dag.png", height = 6, width = 8, units = "in", dpi = 600)
+
+p_sinusitis_abx_dag |> 
+  ggsave(plot = _, filename = "figs/p_sinusitis_abx_dag.pdf", height = 6, width = 8, units = "in")
+
+
+
+#' ###################################
+#' paths to appropriateness
+#' ###################################
 
 #' unadjusted:
 dagitty::paths(sinusitis_abx_dag, from = "pt_sx", to = "appropriate", Z = NULL, directed = FALSE) |> 
@@ -63,3 +77,38 @@ dagitty::paths(sinusitis_abx_dag, from = "pt_sx", to = "appropriate", Z = "Rx", 
   gt::opt_row_striping() |> 
   identity() -> sinusitis_abx_dag_Z_Rx
 sinusitis_abx_dag_Z_Rx
+
+
+
+#' ###################################
+#' paths to Rx
+#' ###################################
+
+#' unadjusted:
+dagitty::paths(sinusitis_abx_dag, from = "pt_sx", to = "Rx", Z = NULL, directed = FALSE) |> 
+  tibble::as_tibble() |> 
+  mutate(open = case_when(open == TRUE ~ "Open",
+                          open == FALSE ~ "Closed")) |> 
+  gt::gt() |> 
+  gt::cols_label(paths = gt::md("Paths symptoms -> appropriate"), open = "Status") |> 
+  gt::tab_style(style = list(gt::cell_text(style = "italic", color = "red")),
+                locations = gt::cells_body(columns = c(open),
+                                           rows = open == "Open")) |> 
+  gt::opt_row_striping() |> 
+  identity() -> sinusitis_abx_Rx_dag_open
+sinusitis_abx_Rx_dag_open
+
+
+#' conditioned on appropriate:
+dagitty::paths(sinusitis_abx_dag, from = "pt_sx", to = "Rx", Z = "appropriate", directed = FALSE) |> 
+  tibble::as_tibble() |> 
+  mutate(open = case_when(open == TRUE ~ "Open",
+                          open == FALSE ~ "Closed")) |> 
+  gt::gt() |> 
+  gt::cols_label(paths = gt::md("Paths symptoms -> appropriate (conditioned on Rx)"), open = "Status") |> 
+  gt::tab_style(style = list(gt::cell_text(style = "italic", color = "red")),
+                locations = gt::cells_body(columns = c(open),
+                                           rows = open == "Open")) |> 
+  gt::opt_row_striping() |> 
+  identity() -> sinusitis_abx_Rx_dag_Z_appropriate
+sinusitis_abx_Rx_dag_Z_appropriate
